5d47dbd73541c3895f0e8ce0f2fc80c5ef53b823
Jesus <jesusk01@gmail.com>
2022-02-18 08:13:47 -0500

    refactor: format invoices

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -16,15 +16,15 @@ interface InvoiceFromCSV {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const formatInvoice = (invoiceFromCSV: {[key: string]: any}): Invoice => {
+export const formatInvoice = (invoiceJSON: {[key: string]: any}): Invoice => {
   const numberFields = [ 'INVOICE_ID', 'VENDOR_ID', 'INVOICE_TOTAL', 'PAYMENT_TOTAL', 'CREDIT_TOTAL', 'BANK_ID' ]
   const dateFields = [ 'INVOICE_DATE', 'INVOICE_DUE_DATE', 'PAYMENT_DATE' ]
 
   const obj = <Invoice>{}
 
-  for (const key in invoiceFromCSV) 
-    if (Object.prototype.hasOwnProperty.call(invoiceFromCSV, key)) {
-      let value = invoiceFromCSV[key]
+  for (const key in invoiceJSON) 
+    if (Object.prototype.hasOwnProperty.call(invoiceJSON, key)) {
+      let value = invoiceJSON[key]
 
       if(value)
         if(numberFields.includes(key))
