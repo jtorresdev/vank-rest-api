a2b7f19cceda419f4810b60c8703de9279b3f918
Jesus <jesusk01@gmail.com>
2022-02-18 02:30:44 -0500

    build: config files

--- config/test.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-export default {
-  dbUri            : process.env.MONGO_URI_TEST,
-  currencyConverter: {
-    baseUri: process.env.CURRENCY_CONVERTER_API,
-    token  : process.env.CURRENCY_CONVERTER_TOKEN
-  }
-}
